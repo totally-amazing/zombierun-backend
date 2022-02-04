d238b39e1855301ac45510cbb794d831b1983667
hyemin <hmpkk09@gmail.com>
2022-02-04 21:04:30 +0900

    feat: implement signIn

--- config.js
+++ config.js
@@ -17,11 +17,16 @@ const config = {
     host: getEnv('DB_HOST'),
   },
   google: {
-    aindroidClientId: getEnv('ANDROID_CLIENT_ID'),
+    clientId: getEnv('GOOGLE_CLIENT_ID'),
   },
   port: Number(getEnv('PORT', 8080)),
   cors: {
     allowOrigin: getEnv('CORS_ALLOW_ORIGIN'),
+  },
+  jwt: {
+    sercetKey: getEnv('JWT_SECRET_KEY'),
+    accessTokenExpiresIn: getEnv('JWT_ACCESS_TOKEN_EXPIRES_IN'),
+    refreshTokenExpiresIn: getEnv('JWT_REFRESH_TOKEN_EXPIRES_IN'),
   },
 };
 
---
+++ service/oAuth.js
@@ -0,0 +1,27 @@
+const { OAuth2Client } = require('google-auth-library');
+const config = require('../config');
+
+class OAuthService {
+  static getAccountFromIdToken = async (idToken) => {
+    const { clientId } = config.google;
+    const client = new OAuth2Client(clientId);
+
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: clientId,
+    });
+
+    const payload = ticket.getPayload();
+
+    return payload && payload.email
+      ? {
+          uid: payload.sub,
+          email: payload.email,
+          imageUrl: payload.picture,
+          nickname: payload.name,
+        }
+      : null;
+  };
+}
+
+module.exports = OAuthService;
