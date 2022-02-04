c730a5489de639d2f7d0517ea58106c029baf0ab
Vaatz <wanmo38@gmail.com>
2022-02-05 02:26:08 +0900

    fix: reflect feedback and remake logic

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -4,82 +4,81 @@ const User = require('../models/User');
 
 const TOKEN_EXPIRED_ERROR = 'TokenExpiredError';
 
-module.exports = async (req, res, next) => {
+const vertifyToken = async () => (req, res, next) => {
   if (req.method === 'OPTIONS') {
     return next();
   }
 
-  let user;
-  let accessToken;
-  let decodedAccessToken;
-  let refreshToken;
-  let decodedRefreshToken;
-
   try {
-    accessToken = req.headers.authorization.split(' ')[1];
 
-    decodedAccessToken = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+    const accessToken = req.headers.authorization.split(' ')[1];
+
+    const decodedAccessToken = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
 
     const { id, nickname, imageUrl } = decodedAccessToken;
 
-    user = await User.findById(id);
+    const user = await User.findById(id);
 
-    refreshToken = user.refreshToken;
+    const refreshToken = user.refreshToken;
 
-    try {
-      decodedRefreshToken = jwt.verify(user.refreshToken, process.env.JWT_SECRET_KEY);
+    jwt.verify(refreshToken, process.env.JWT_SECRET_KEY, async (error, decoded) => {
+      if (error) {
+        if (error.name === TOKEN_EXPIRED_ERROR) {
+          const newRefreshToken = jwt.sign({
+            id,
+            nickname,
+            imageUrl,
+          }, process.env.JWT_ACCESS_TOKEN_EXPIRES_IN, {
+            expiresIn: Number(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN)
+          });
 
-    } catch (error) {
-      if (error.name = TOKEN_EXPIRED_ERROR) {
-        const newRefreshToken = jwt.sign({
+          user.refreshToken = newRefreshToken;
+          await user.save();
+          req.user = { id, nickname, imageUrl };
+          return next();
+        } else {
+          error.status = 401;
+          return next(error);
+        }
+      }
+
+      return next();
+    });
+
+  } catch (error) {
+
+    if (error.name === TOKEN_EXPIRED_ERROR) {
+      const expiredDecodedAccessToken =
+        jwt.verify(accessToken, process.env.JWT_SECRET_KEY, { ignoreExpiration: true });
+
+      const { id, nickname, imageUrl } = expiredDecodedAccessToken;
+
+      const user = await User.findById(id);
+
+      const refreshToken = user.refreshToken;
+
+      jwt.verify(refreshToken, process.env.JWT_SECRET_KEY, (error, decoded) => {
+        if (error) {
+          error.status = 401;
+          return next(error);
+        }
+
+        const newAccessToken = jwt.sign({
           id,
           nickname,
           imageUrl,
-        }, process.env.JWT_ACCESS_TOKEN_EXPIRES_IN, {
-          expiresIn: Number(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN)
+        }, process.env.JWT_SECRET_KEY, {
+          expiresIn: Number(process.env.JWT_ACCESS_TOKEN_EXPIRES_IN)
         });
 
-        user.refreshToken = newRefreshToken;
-        await user.save();
-        req.user = { id, nickname, imageUrl };
-        next();
-      }
+        req.token = newAccessToken;
+        return next();
+      })
+    } else {
+      err.status = 401;
+      return next(err);
     }
-
-    req.user = { id, nickname, imageUrl };
-    next();
-  } catch (error) {
-
-    if (error.name = TOKEN_EXPIRED_ERROR) {
-      decoded = jwt.verify(accessToken, process.env.JWT_SECRET_KEY, { ignoreExpiration: true });
-
-      const { id, nickname, imageUrl } = decodedAccessToken;
-
-      try {
-        decodedRefreshToken = jwt.verify(user.refreshToken, process.env.JWT_SECRET_KEY);
-
-      } catch (error) {
-        const err = new Error(ERROR.AUTHENTICATION_FAILED);
-
-        err.status = 401;
-        return next(err);
-      }
-
-      const newAccessToken = jwt.sign({
-        id,
-        nickname,
-        imageUrl,
-      }, process.env.JWT_SECRET_KEY, {
-        expiresIn: Number(process.env.JWT_ACCESS_TOKEN_EXPIRES_IN)
-      });
-
-      req.token = newAccessToken;
-      next();
-    }
-
-    const err = new Error(ERROR.AUTHENTICATION_FAILED);
-
-    err.status = 401;
-    return next(err);
   }
 };
+
+module.exports = vertifyToken;
