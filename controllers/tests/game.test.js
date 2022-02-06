fbe8d976e9cd50fb4021018634c6148f564bae50
hyemin <hmpkk09@gmail.com>
2022-02-06 13:56:59 +0900

    test: update getTotalRecord test

--- controllers/tests/game.test.js
+++ controllers/tests/game.test.js
@@ -1,24 +1,28 @@
 const { createRequest, createResponse } = require('node-mocks-http');
 
-const Game = require('../../models/Game');
+const { getGamesByUserId } = require('../../models/Game');
 const dummyUser = require('../../models/sample_user.json')[1];
 const dummyGames = require('../../models/sample_game.json');
 const { getTotalRecord } = require('../game');
 
-jest.mock('../../models/Game');
+jest.mock('../../models/Game', () => ({
+  getGamesByUserId: jest.fn(),
+}));
 
 describe('Game Controller', () => {
   describe('getTotalRecord', () => {
-    it('parameter로 받은 id와 일치하는 유저의 total record를 응답한다.', async () => {
+    it('query로 받은 id와 일치하는 유저의 total record를 응답한다.', async () => {
       const req = createRequest({
         query: {
           userId: dummyUser._id,
         },
       });
       const res = createResponse();
-      Game.find = jest.fn(() => ({
-        lean: async () => dummyGames,
-      }));
+      getGamesByUserId.mockResolvedValue(
+        dummyGames.filter(({ players }) =>
+          players.find(({ id }) => id === dummyUser._id)
+        )
+      );
 
       await getTotalRecord(req, res);
 
