00a696de3404aaa7a9ec0d90de489e54c666866d
hyemin <hmpkk09@gmail.com>
2022-02-08 23:05:14 +0900

    feat: validate request body of 'POST /room'

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -20,3 +20,43 @@ exports.validateQuery = async (req, res, next) => {
 
   return next();
 };
+
+exports.validateRoom = (req, res, next) => {
+  const { mode, title, speed, time } = req.body;
+
+  if (!mode) {
+    throw new Error('mode가 없습니다');
+  }
+
+  if (!['survival', 'oneOnOne', 'solo'].includes(mode)) {
+    throw new Error('mode는 survival, oneOnOne, solo 중에 하나여야 합니다');
+  }
+
+  req.body.title = req.body.title.trim();
+
+  if (!req.body.title) {
+    throw new Error('title이 없습니다');
+  }
+
+  if (!(typeof title === 'string')) {
+    throw new Error('title은 문자열이어야 합니다');
+  }
+
+  if (speed && !(typeof speed === 'number')) {
+    throw new Error('speed는 숫자여야 합니다');
+  }
+
+  if (speed && (speed < 1 || speed > 20)) {
+    throw new Error('speed는 1 이상 20 이하여야 합니다');
+  }
+
+  if (time && !(typeof time === 'number')) {
+    throw new Error('time은 숫자여야 합니다');
+  }
+
+  if (time && !(time < 30 || time > 2000)) {
+    throw new Error('time은 30 이상 2000이하여야 합니다');
+  }
+
+  next();
+};
--- routes/room.js
+++ routes/room.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const roomController = require('../controllers/room');
+const { validateRoom } = require('../middlewares/validators');
 
 const router = express.Router();
 
 router.get('/', roomController.getRoomList);
-router.post('/', roomController.createRoom);
+router.post('/', validateRoom, roomController.createRoom);
 
 module.exports = router;
