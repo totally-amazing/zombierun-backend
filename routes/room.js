697b572eb223ee94873ba9df9e545215093c53e9
hyemin <hmpkk09@gmail.com>
2022-02-08 21:05:30 +0900

    feat: delete APIs routes superseded by sockets

--- routes/room.js
+++ routes/room.js
@@ -6,7 +6,5 @@ const router = express.Router();
 
 router.get('/', roomController.getRoomList);
 router.post('/', roomController.createRoom);
-router.put('/:id/enter', (req, res, next) => {});
-router.delete('/:id/exit', (req, res, next) => {});
 
 module.exports = router;
