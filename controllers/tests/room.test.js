3c3c79bb2a2b172d3853e2714157ada966482c87
hyemin <hmpkk09@gmail.com>
2022-02-08 20:59:53 +0900

    test: createRoom

--- controllers/tests/room.test.js
+++ controllers/tests/room.test.js
@@ -2,7 +2,7 @@ const { createRequest, createResponse } = require('node-mocks-http');
 
 const dummyRooms = require('../../models/sample_room.json');
 const Room = require('../../models/Room');
-const { getRoomList } = require('../room');
+const { getRoomList, createRoom } = require('../room');
 
 jest.mock('../../models/Room');
 
@@ -19,6 +19,29 @@ describe('Room Cotroller', () => {
       await getRoomList(req, res);
 
       expect(res._getData()).toEqual(dummyRooms);
+    });
+  });
+
+  describe('createRoom', () => {
+    it('새로운 room을 생성하고 room id로 응답한다', async () => {
+      const room = {
+        mode: 'mode',
+        title: 'title',
+        speed: 'speed',
+        time: 'time',
+      };
+      const req = createRequest({
+        url: '/room',
+        method: 'POST',
+        body: room,
+      });
+      const res = createResponse();
+      Room.create = jest.fn(async () => ({ id: 'id' }));
+
+      await createRoom(req, res);
+
+      expect(Room.create).toBeCalledWith({ ...room, participants: [] });
+      expect(res._getData()).toEqual({ id: 'id' });
     });
   });
 });
