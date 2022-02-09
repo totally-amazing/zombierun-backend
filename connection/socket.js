ad318b605441ad2fe079507db4fcfb0cf4db5c75
hyemin <hmpkk09@gmail.com>
2022-02-09 13:38:42 +0900

    refactor: extract listeners from socket class

--- connection/socket.js
+++ connection/socket.js
@@ -1,87 +1,33 @@
 const { Server } = require('socket.io');
 
+const SocketListener = require('../service/socket');
 const config = require('../config');
 
 class Socket {
-  constructor(server, roomModel) {
+  constructor(server) {
     this.io = new Server(server, {
       cors: {
         origin: config.cors.allowOrigin,
       },
     });
+    this.listener = new SocketListener(this.io);
 
     this.io.on('connect', (socket) => {
       console.log(`socket::::: ${socket.id} connected`);
 
-      listenJoinRoom(socket, roomModel);
-      listenUserReady(socket);
-      listenUserNotReady(socket);
-      listenUserLeave(socket, roomModel);
-      listenUserDie(socket);
+      this.listener.joinRoom(socket);
+      this.listener.ready(socket);
+      this.listener.notReady(socket);
+      this.listener.leaveRoom(socket);
+      this.listener.die(socket);
     });
   }
 }
 
-let socketServer;
+let socket;
 
 exports.initSocket = (server) => {
-  if (!socketServer) {
-    socketServer = new Socket(server);
+  if (!socket) {
+    socket = new Socket(server);
   }
 };
-
-function listenJoinRoom(socket, roomModel) {
-  socket.on('user/join', async (roomId, userId) => {
-    console.log(`socket::::: user ${userId} joined room`);
-
-    socket.join(roomId);
-    await roomModel.findByIdAndUpdate(roomId, {
-      $push: { participants: userId },
-    });
-
-    socket.roomId = roomId;
-    socket.userId = userId;
-
-    this.io.to(roomId).emit('user/join', userId);
-  });
-}
-
-function listenUserReady(socket) {
-  socket.on('user/ready', () => {
-    console.log(`socket::::: user ${socket.userId} is ready`);
-
-    this.io.to(socket.roomId).emit('user/ready', socket.userId);
-  });
-}
-
-function listenUserNotReady(socket) {
-  socket.on('user/notReady', () => {
-    console.log(`socket::::: user ${socket.userId} is not ready`);
-
-    this.io.to(socket.roomId).emit('user/notReady', socket.userId);
-  });
-}
-
-function listenUserLeave(socket, roomModel) {
-  socket.on('user/leave', async () => {
-    console.log(`socket::::: user ${socket.userId} left room`);
-
-    const room = await roomModel.findById(socket.roomId);
-    room.participants = room.participants.filter(
-      (userId) => userId !== socket.userId
-    );
-    await room.save();
-
-    this.io.to(socket.roomId).emit('user/leave', socket.userId);
-    socket.leave(socket.roomId);
-  });
-}
-
-function listenUserDie(socket) {
-  socket.on('user/die', () => {
-    console.log(`socket::::: user ${socket.userId} is dead`);
-
-    this.io.to(socket.roomId).emit('user/die', socket.userId);
-    socket.leave(socket.roomId);
-  });
-}
---
+++ service/socket.js
@@ -0,0 +1,104 @@
+const Room = require('../models/Room');
+
+class SocketListener {
+  constructor(io) {
+    this.io = io;
+  }
+
+  joinRoom = (socket) => {
+    socket.on('user/join', async (roomId, userId) => {
+      if (socket.roomId) {
+        console.error('이미 join된 room이 있습니다');
+        return;
+      }
+
+      try {
+        const room = await Room.findById(roomId);
+        room.participants.push(userId);
+        await room.save();
+
+        socket.roomId = roomId;
+        socket.userId = userId;
+
+        socket.join(roomId);
+        this.io.to(roomId).emit('user/join', userId);
+
+        console.log(`socket::::: user ${userId} joined room`);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  };
+
+  ready = (socket) => {
+    socket.on('user/ready', () => {
+      try {
+        this.checkIfJoinRoom(socket);
+
+        this.io.to(socket.roomId).emit('user/ready', socket.userId);
+
+        console.log(`socket::::: user ${socket.userId} is ready`);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  };
+
+  notReady = (socket) => {
+    socket.on('user/notReady', () => {
+      try {
+        this.checkIfJoinRoom(socket);
+
+        this.io.to(socket.roomId).emit('user/notReady', socket.userId);
+
+        console.log(`socket::::: user ${socket.userId} is not ready`);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  };
+
+  leaveRoom = (socket) => {
+    socket.on('user/leave', async () => {
+      try {
+        this.checkIfJoinRoom(socket);
+
+        const room = await Room.findById(socket.roomId);
+        room.participants.remove(socket.userId);
+        await room.save();
+
+        socket.roomId = null;
+
+        this.io.to(socket.roomId).emit('user/leave', socket.userId);
+        socket.leave(socket.roomId);
+
+        console.log(`socket::::: user ${socket.userId} left room`);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  };
+
+  die = (socket) => {
+    socket.on('user/die', () => {
+      try {
+        this.checkIfJoinRoom(socket);
+
+        this.io.to(socket.roomId).emit('user/die', socket.userId);
+        socket.leave(socket.roomId);
+
+        console.log(`socket::::: user ${socket.userId} is dead`);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  };
+
+  checkIfJoinRoom = (socket) => {
+    if (!socket.roomId) {
+      throw new Error('socket이 room에 join되지 않았습니다');
+    }
+  };
+}
+
+module.exports = SocketListener;
