dc1568a7f33f25f0a8e70bf11d29acc0381c13fd
hyemin <hmpkk09@gmail.com>
2022-02-18 23:42:39 +0900

    feat: add GET auth/me

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const { ERROR } = require('../constants');
 const User = require('../models/User');
 const OAuthService = require('../service/oAuth');
 const TokenService = require('../service/token');
@@ -19,6 +23,7 @@ exports.signIn = async (req, res, next) => {
   }
 
   const tokenService = new TokenService();
+
   tokenService.setUser(user);
 
   const accessToken = tokenService.createAccessToken();
@@ -31,5 +36,41 @@ exports.signIn = async (req, res, next) => {
     nickname: user.nickname,
     imageUrl: user.imageUrl,
     token: accessToken,
+  });
+};
+
+exports.me = async (req, res) => {
+  const { userId } = req;
+  const user = await User.findById(userId);
+
+  if (!user) {
+    return res.status(404).send(ERROR.USER_NOT_FOUND);
+  }
+
+  try {
+    jwt.verify(user.refreshToken, config.jwt.sercetKey);
+  } catch (error) {
+    if (error.name === ERROR.TOKEN_EXPIRED) {
+      const tokenService = new TokenService();
+      tokenService.setUser(user);
+      const refreshToken = tokenService.createRefreshToken();
+      await User.findByIdAndUpdate(user.id, { refreshToken });
+
+      return res.send({
+        token: req.token,
+        user: {
+          id: user.id,
+          nickname: user.nickname,
+          imageUrl: user.imageUrl,
+        },
+      });
+    }
+  }
+
+  res.send({
+    token: req.token,
+    id: user.id,
+    nickname: user.nickname,
+    imageUrl: user.imageUrl,
   });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const authController = require('../controllers/auth');
+const { checkAuth } = require('../middlewares/checkAuth');
 
 const router = express.Router();
 
 router.post('/signin', authController.signIn);
+router.get('/me', checkAuth, authController.me);
 
 module.exports = router;
