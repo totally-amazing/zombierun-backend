38e7380a37ba0a22f374298781884ef1d1d3ffd7
hyemin <hmpkk09@gmail.com>
2022-02-18 10:46:15 +0900

    feat: update validators

--- utils/validator.js
+++ utils/validator.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const User = require('../models/User');
 
 function required(array) {
-  if (!array.every((value) => value !== undefined)) {
-    throw new Error(`array의 undefined가 존재합니다. ${array}`);
+  if (array.every((value) => value == null)) {
+    throw new Error(`값이 존재하지 않습니다. value: ${array}`);
   }
 }
 
@@ -12,7 +12,7 @@ function validateId(id) {
   required([id]);
 
   if (!mongoose.isValidObjectId(id)) {
-    throw new Error('유효하지 않은 id입니다');
+    throw new Error(`유효하지 않은 id입니다: ${id}`);
   }
 }
 
@@ -20,7 +20,7 @@ async function findExistentUserId(userId) {
   const user = await User.findById(userId);
 
   if (!user) {
-    throw new Error('해당 userId를 찾을 수 없습니다');
+    throw new Error(`해당 user를 찾을 수 없습니다 user id : ${userId}`);
   }
 }
 
@@ -32,25 +32,27 @@ function checkTypeOfNumber(number) {
 
 function checkTypeOfString(string) {
   if (!(typeof string === 'string')) {
-    throw new Error('글자가 아닙니다');
+    throw new Error(`${string}은 문자열이 아닙니다`);
   }
 }
 
 function checkTypeOfBoolean(boolean) {
   if (!(typeof boolean === 'boolean')) {
-    throw new Error('불리언이 아닙니다');
+    throw new Error(`${boolean}은 불리언이 아닙니다`);
   }
 }
 
 function checkTypeOfMode(mode) {
   if (!['survival', 'oneOnOne', 'solo'].includes(mode)) {
-    throw new Error('mode는 survival, oneOnOne, solo 중에 하나여야 합니다');
+    throw new Error(
+      `mode는 survival, oneOnOne, solo 중에 하나여야 합니다 mode: ${mode}`
+    );
   }
 }
 
 function checkTypeOfRole(role) {
   if (!['human', 'zombie'].includes(role)) {
-    throw new Error('role은 human, zombie 중에 하나여야 합니다');
+    throw new Error(`role은 human, zombie 중에 하나여야 합니다 role: ${role}`);
   }
 }
 
