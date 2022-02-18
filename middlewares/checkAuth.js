0d2eb906b4d52ecc792568bd8a1e7406460812e3
hyemin <hmpkk09@gmail.com>
2022-02-18 23:37:41 +0900

    feat: update checkAuth

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -1,97 +1,49 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
-const TOKEN_EXPIRED_ERROR = 'TokenExpiredError';
+const config = require('../config');
+const { ERROR } = require('../constants');
+const TokenService = require('../service/token');
 
-const vertifyToken = async (req, res, next) => {
+const createAuthMiddleware = (tokenService) => async (req, res, next) => {
+  const authHeader = req.get('Authorization');
+  console.log(authHeader);
+
+  if (!(authHeader && authHeader.startsWith('Bearer '))) {
+    return res.status(401).send(ERROR.AUTHENTICATION_FAILED);
+  }
+
+  const accessToken = authHeader.split(' ')[1];
+
+  if (!accessToken) {
+    return res.status(401).send(ERROR.AUTHENTICATION_FAILED);
+  }
+
   try {
-    const accessToken = req.headers.authorization.split(' ')[1];
+    const { id } = jwt.verify(accessToken, config.jwt.sercetKey);
+    req.userId = id;
+    req.token = accessToken;
 
-    const decodedAccessToken = jwt.verify(
-      accessToken,
-      process.env.JWT_SECRET_KEY
-    );
-
-    const { id, nickname, imageUrl } = decodedAccessToken;
-
-    const user = await User.findById(id);
-
-    const { refreshToken } = user;
-
-    jwt.verify(
-      refreshToken,
-      process.env.JWT_SECRET_KEY,
-      async (error, decoded) => {
-        if (error) {
-          if (error.name === TOKEN_EXPIRED_ERROR) {
-            const newRefreshToken = jwt.sign(
-              {
-                id,
-                nickname,
-                imageUrl,
-              },
-              process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
-              {
-                expiresIn: Number(process.env.JWT_REFRESH_TOKEN_EXPIRES_IN),
-              }
-            );
-
-            user.refreshToken = newRefreshToken;
-            await user.save();
-            req.user = { id, nickname, imageUrl };
-            return next();
-          }
-          error.status = 401;
-          return next(error);
-        }
-
-        req.user = { id, nickname, imageUrl };
-        return next();
-      }
-    );
+    next();
   } catch (error) {
-    const accessToken = req.headers.authorization.split(' ')[1];
-
-    if (error.name === TOKEN_EXPIRED_ERROR) {
-      const expiredDecodedAccessToken = jwt.verify(
-        accessToken,
-        process.env.JWT_SECRET_KEY,
-        { ignoreExpiration: true }
-      );
-
-      const { id, nickname, imageUrl } = expiredDecodedAccessToken;
-
-      const user = await User.findById(id);
-
-      const { refreshToken } = user;
-
-      jwt.verify(refreshToken, process.env.JWT_SECRET_KEY, (err, decoded) => {
-        if (err) {
-          err.status = 401;
-          return next(err);
-        }
-
-        const newAccessToken = jwt.sign(
-          {
-            id,
-            nickname,
-            imageUrl,
-          },
-          process.env.JWT_SECRET_KEY,
-          {
-            expiresIn: Number(process.env.JWT_ACCESS_TOKEN_EXPIRES_IN),
-          }
-        );
-
-        req.token = newAccessToken;
-        req.user = { id, nickname, imageUrl };
-        return next();
-      });
-    } else {
-      error.status = 401;
-      return next(error);
+    if (error.name !== ERROR.TOKEN_EXPIRED) {
+      return res.status(401).send(ERROR.AUTHENTICATION_FAILED);
     }
+
+    const user = await tokenService.getUserByExpiredToken(accessToken);
+
+    if (!(user && tokenService.isTokenValid(user.refreshToken))) {
+      return res.status(401).send(ERROR.AUTHENTICATION_FAILED);
+    }
+
+    tokenService.setUser(user);
+    const newAccessToken = tokenService.createAccessToken();
+    req.userId = user.id;
+    req.token = newAccessToken;
+
+    next();
   }
 };
+const tokenService = new TokenService();
 
-module.exports = vertifyToken;
+exports.createAuthMiddleware = createAuthMiddleware;
+exports.checkAuth = createAuthMiddleware(tokenService);
